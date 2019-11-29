adf6e66b00ccc413eb077cffeae29da135dd2840
TSKangetsu <x403070567@live.com>
2019-11-29 11:47:36 +0800

    cpu bind thread add

--- SingleAPM.cpp
+++ SingleAPM.cpp
@@ -5,28 +5,35 @@ int main()
 	long int timer;
 	long int timer_end;
 	Stablize_Mode test;
-	test.ControlCalibration();
-	test.SensorsGryoCalibration();
-	while (true)
-	{
-		timer = micros();
-		test.SensorsParse();
-		test.ControlParse();
-		test.AttitudeUpdate();
-		test.MotorUpdate();
-		//---------test-----------//
-		std::cout << _uORB_B1_Speed << " ";
-		std::cout << _uORB_A1_Speed << " ";
-		std::cout << _uORB_A2_Speed << " ";
-		std::cout << _uORB_B2_Speed << " ";
-		//----------test----------//
-		timer_end = micros();
-		if ((timer_end - timer) > 4000)
+	std::thread AutoLevelingMain([&] {
+		test.ControlCalibration();
+		test.SensorsGryoCalibration();
+		while (true)
 		{
-			std::cout << "[WARING!!!!] Frequency Sync error , Over 4ms !!!!! Dangours !!! Gryo Angle error !!!!";
-			_flag_ForceFailed_Safe = true;
+			timer = micros();
+			test.SensorsParse();
+			test.ControlParse();
+			test.AttitudeUpdate();
+			test.MotorUpdate();
+			//---------test-----------//
+			std::cout << _uORB_B1_Speed << " ";
+			std::cout << _uORB_A1_Speed << " ";
+			std::cout << _uORB_A2_Speed << " ";
+			std::cout << _uORB_B2_Speed << " ";
+			//----------test----------//
+			timer_end = micros();
+			if ((timer_end - timer) > 4000)
+			{
+				std::cout << "[WARING!!!!] Frequency Sync error , Over 4ms !!!!! Dangours !!! Gryo Angle error !!!!";
+				_flag_ForceFailed_Safe = true;
+			}
+			std::cout << "timer : " << timer_end - timer << "\n";
+			usleep(3990 - (timer_end - timer));
 		}
-		std::cout << "timer : " << timer_end - timer << "\n";
-		usleep(3990 - (timer_end - timer));
-	}
+		});
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(3, &cpuset);
+	int rc = pthread_setaffinity_np(AutoLevelingMain.native_handle(), sizeof(cpu_set_t), &cpuset);
+	AutoLevelingMain.join();
 }
